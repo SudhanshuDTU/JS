c9dba78f9d3a76a1957d36951caebe204b5d6dbe
Sudhanshu jha <116909414+SudhanshuDTU@users.noreply.github.com>
2023-10-29 22:19:25 +0530

    Add files via upload

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553; //value can't change 
+let accountEmail = "Sudhanshujha2717@gmail.com"; //value can be change
+var accountPassword = "12345678"; //value can be change
+// we don't use var because of issue in Scope
+
+accountCity = "Jaipur"; // kr skte hai pr not good practice
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all Js Code as New version
+
+// alert(" Hi ")  we are using Nodejs, not browser
+
+let name = "Sudhanshu";  //string
+let age = 21;            //number
+let isLoggedIn = true;   //bool
+
+//null => Stand Alone value hai , empty value ka representation hai
+//symbol => unique
+//undefined => variable declare krke jb value nhi dete to undefined hota hai
+//object
+
+console.log(typeof(name)); //string
+
+console.log(typeof(undefined)); // undefined
+console.log(typeof(null));  // Object
